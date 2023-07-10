83227172f82b2322b7578ac2d45b668af8d119d1
HassanKourani <hkourani36@gmail.com>
2023-07-11 00:34:05 +0300

    added basic logic to the game functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Singup from "./Interfaces/Singup";
+import Main from "./Interfaces/Main";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Singup />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </Router>
   );
--- src/Interfaces/Singup.tsx
+++ src/Interfaces/Singup.tsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Singup = () => {
+  const [username, setUsername] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const navigate = useNavigate();
+  const handleSignup = () => {
+    username.length < 3
+      ? setError("username must be more than 3 chars.")
+      : navigate("/main", { state: { username: username } });
+  };
   return (
     <div className="center h-screen">
       <div className="bg-white border-2 border-black h-3/4 w-1/4 rounded-md p-4 flex justify-center">
         <h3>Sign Up</h3>
         <div className="">
-          <input name="username" className="border border-gray-800" />
-          <button>Submit</button>
+          <input
+            name="username"
+            className="border border-gray-800"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={handleSignup}>Submit</button>
+          <>{error}</>
         </div>
       </div>
     </div>
