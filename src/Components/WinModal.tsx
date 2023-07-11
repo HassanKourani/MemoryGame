b2820ff17d27ce14930aa6d9ae05e86c39b0ffcf
HassanKourani <hkourani36@gmail.com>
2023-07-11 17:21:24 +0300

    fixed double click bug

--- src/Components/WinModal.tsx
+++ src/Components/WinModal.tsx
@@ -2,9 +2,9 @@ import PlayAgainBtn from "./PlayAgainBtn";
 
 const WinModal = () => {
   return (
-    <div className="fixed h-screen inset-0 flex justify-center items-center">
-      <div className="h-60 w-1/3 bg-orange-100 border-4 border-darkBlue-100 rounded-xl text-white flex flex-col items-center justify-around">
-        <span className="text-2xl font-bold">CONGRATULATIONS YOU WON!!</span>
+    <div className="fixed h-screen inset-0 flex justify-center items-center bg-black/90">
+      <div className="win-modal">
+        <span className="text-xl font-bold">CONGRATULATIONS YOU WON!!</span>
         <span className="text-xl ">number of attempts: 12</span>
         <PlayAgainBtn />
       </div>
